526a042fb3d11b07f48bd9089343fcc9fa6d2caa
Rui Santos <randomnerdtutorials@gmail.com>
2020-09-09 14:38:54 +0100

    Update DHT_Example.ino

--- Code/PART1_Arduino/Module3/Unit1/DHT_Example/DHT_Example.ino
+++ Code/PART1_Arduino/Module3/Unit1/DHT_Example/DHT_Example.ino
@@ -5,7 +5,7 @@
 
 #include "DHT.h"
 
-#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
